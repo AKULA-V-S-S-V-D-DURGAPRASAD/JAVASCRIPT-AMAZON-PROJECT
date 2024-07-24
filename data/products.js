342acf3e0051a893d9015a7fb67c32bf4dcbd741
DurgaPrasad Akula <akuladurgaprasad5@gmail.com>
2024-07-24 10:29:13 +0530

    Convert products into Classes

--- data/products.js
+++ data/products.js
@@ -1,3 +1,5 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
 export function getProduct(productId) {
   let matchingProduct;
 
@@ -10,8 +12,32 @@ export function getProduct(productId) {
     return matchingProduct;
 }
 
+class product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+getPrice() {
+ return `$${formatCurrency(this.priceCents)}`;
+}
+}
+
 export const products = [
-  {
+ {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -669,4 +695,6 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails) => {
+  return new product(productDetails);
+});
